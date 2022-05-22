50889db8b178dea0dbee0e8b6be05f10a8f70cb1
pasindu <it19390260@my.sliit.lk>
2022-05-22 18:13:40 +0530

    add imports

--- model3.py
+++ model3.py
@@ -1,3 +1,13 @@
+# In[1]:
+# import required packages
+from sklearn.model_selection import train_test_split
 import data_prep
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn import metrics
 
 data_prep.vehicle_dataset.head()
+
+## In[2]:
