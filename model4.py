cfc7b3feb36089ae21be681a4110ecd18c7de4c4
it19020822 <it19020822@my.sliit.lk>
2022-05-22 16:35:15 +0530

    model 4 added with data_prep file changes

---
+++ model4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+# import required packages
+import data_prep
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn import metrics
+
+# In[2]:
