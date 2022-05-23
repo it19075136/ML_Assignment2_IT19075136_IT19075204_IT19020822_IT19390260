2c24ea25bb8058484f6e4d88c65d814d57f254f7
it19075204 <it19075204@my.sliit.lk>
2022-05-23 10:13:36 +0530

    added imports to the model

---
+++ model2.py
@@ -0,0 +1,11 @@
+#In[1]
+
+#import required packages
+from numpy import matrix
+from sklearn.model_selection import train_test_split
+import data_prep
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn import metrics
