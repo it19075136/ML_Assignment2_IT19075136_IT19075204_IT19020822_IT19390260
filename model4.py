02b88acae669e45fc956f652015166a3615f2e0a
it19020822 <it19020822@my.sliit.lk>
2022-05-23 22:45:04 +0530

    lasso model plots calculations added

--- model4.py
+++ model4.py
@@ -4,10 +4,8 @@
 # In[1]:
 
 # import required packages
-from numpy import matrix
 from sklearn.model_selection import train_test_split
 import data_prep
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
@@ -103,6 +101,74 @@ print("R Squre Error : ", error_score)
 # In[13]:
 
 
+#### Visualize the actual prices vs predicted prices ####
+plt.scatter(Y_test, test_data_pred)
+plt.xlabel("Actual Selling Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Price Vs Predicted Prices")
+plt.show()
+
+
+############## 2. Lasso Regression ################
+# In[14]:
+
+
+# loading the lasso regression model
+lass_reg_model = Lasso()
+
+
+# In[15]:
+
+
+# fit() training data to the lasso regression model
+lass_reg_model.fit(X_train, Y_train)
+
+
+#### Model Evaluation ####
+# In[16]:
+
+
+# prediction on training data
+train_data_pred = lass_reg_model.predict(X_train)
+
+
+# In[17]:
+
+
+# R squre Error train data
+error_score = metrics.r2_score(Y_train, train_data_pred)
+print("R Squre Error : ", error_score)
+
+
+# In[18]:
+
+
+#### Visualize the actual prices vs predicted prices ####
+plt.scatter(Y_train, train_data_pred)
+plt.xlabel("Actual Selling Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Prices Vs Predicted Prices")
+plt.show()
+
+# In[19]:
+
+
+# prediction on test data
+test_data_pred = lass_reg_model.predict(X_test)
+
+
+# In[20]:
+
+
+# R squre error for test data
+error_score = metrics.r2_score(Y_test, test_data_pred)
+print("R Squre Error : ", error_score)
+
+
+# In[21]:
+
+
+#### Visualize the actual prices vs predicted prices ####
 plt.scatter(Y_test, test_data_pred)
 plt.xlabel("Actual Selling Price")
 plt.ylabel("Predicted Price")
